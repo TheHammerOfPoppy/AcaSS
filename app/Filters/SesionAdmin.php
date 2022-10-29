ab2b0729ae9629f7d9d64d3b6c1bc6377d746082
TheHammerOfPoppy <rasec.cor.al@gmail.com>
2022-10-29 16:16:53 -0500

    Se agreo

---
+++ app/Filters/SesionAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class SesionAdmin implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        if(!session('type') == 'admin' or !session('type') == 'usuario') {
+            
+            return redirect()->to(base_url('/'));
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
